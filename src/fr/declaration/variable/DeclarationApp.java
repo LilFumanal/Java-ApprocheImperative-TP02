1688d3fb69bf2a9da7bc807130fbc5ccc2d6998f
LilFumanal <charlie.fumanal@gmail.com>
2022-10-09 16:24:21 +0200

    first commit after mistake, tp 02, 03, 04

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,17 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+	public static void main(String[] args) {
+		byte b = 10;
+		short s = -16278;
+		int i = 2846325;
+		long l = 1000L;
+		float f = 2.5F;
+		double d = 6.4D;
+		char c = 'c';
+		boolean myBool = true;
+		String str = "coucou";
+		
+		System.out.println(b);
+	}
+}
